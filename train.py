a56d390fec22d8986f5c74bcfbdbdb92b3a2fc0f
Zakki0925224 <zakki0925224@gmail.com>
2025-01-06 21:29:38 +0900

    [Add]: Added the first commit

---
+++ train.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8s.pt")
+model.train(
+    data="project-3-at-2025-01-06-09-00-68834e25.yaml",
+    epochs=50,
+    imgsz=640,
+    device="cpu",
+)
+model.export(format="tflite")
